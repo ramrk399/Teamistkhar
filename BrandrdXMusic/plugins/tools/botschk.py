91ace401fcb5974ff7dcdd17c685e90e45634117
ramrk399 <ramavtarchauhan2000@gmail.com>
2024-08-23 15:01:07 +0530

    Update botschk.py

--- BrandrdXMusic/plugins/tools/botschk.py
+++ BrandrdXMusic/plugins/tools/botschk.py
@@ -3,10 +3,10 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from dotenv import load_dotenv
 import config
-from VIPMUSIC.core.userbot import Userbot
-from VIPMUSIC import app
+from BrandrdXMusic.core.userbot import Userbot
+from BrandrdXMusic import app
 from datetime import datetime
-from VIPMUSIC.utils.database import get_assistant
+from BrandrdXMusic.utils.database import get_assistant
 
 # Assuming Userbot is defined elsewhere
 
